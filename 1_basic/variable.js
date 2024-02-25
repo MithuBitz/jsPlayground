f1afad1a05c484fbab246890d575560a9a89078f
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-02-25 23:46:51 +0530

    variable in JS

---
+++ 1_basic/variable.js
@@ -0,0 +1,23 @@
+// variable in Java script
+
+//Declare const variable_name = value, and this value cannot be change later on
+const facebookId = 1986744;
+
+// facebookId = 5648738; //No yor cannot do this bcoz its declare as a const
+
+//Declare let variable if we want to change its value later on
+let fbUserEmail = "mithu@mit.io";
+
+//Declare var is not commonly used by these days bcoz it has problem on block scope and functional scope
+var fbAccountPass = "nothing";
+
+//we can declare the variable name directly without giveing any data type, but dont use this. But its possible in JS
+fbRecoverEmail = "mit@mit.io";
+
+//We can change the value of
+fbUserEmail = "anurag@mit.io";
+fbAccountPass = "12345678";
+fbRecoverEmail = "mit1@mit.io";
+
+console.log(facebookId);
+console.table([fbUserEmail, fbAccountPass, fbRecoverEmail]);
